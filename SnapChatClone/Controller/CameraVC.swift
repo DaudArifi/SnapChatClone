12f92fe11f89fb55c53936ae595335851f4c5a06
Daud Arifi <daud.arifi@Dauds-MacBook-Pro.local>
2018-03-07 17:15:20 +0100

    first Commit

---
+++ SnapChatClone/Controller/CameraVC.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  SnapChatClone
+//
+//  Created by Daud Arifi on 02.03.18.
+//  Copyright © 2018 Daud Arifi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: AVCamCameraViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
---
+++ SnapChatClone/Protocols/AVCameraVCDelegate.h
@@ -0,0 +1,13 @@
+//
+//  AVCameraVCDelegate.h
+//  SnapChatClone
+//
+//  Created by Daud Arifi on 03.03.18.
+//  Copyright © 2018 Daud Arifi. All rights reserved.
+//
+
+#ifndef AVCameraVCDelegate_h
+#define AVCameraVCDelegate_h
+
+
+#endif /* AVCameraVCDelegate_h */
